669c6a0fea2b36465c610831ffa1ac27c6ab8c87
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-05-20 23:00:30 +0200

    add start, stop & calulate

--- script-compiled.js
+++ script-compiled.js
@@ -5,59 +5,97 @@ var _createClass = function () { function defineProperties(target, props) { for 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Stopwatch = function () {
-    function Stopwatch(display) {
-        _classCallCheck(this, Stopwatch);
+				function Stopwatch(display) {
+								_classCallCheck(this, Stopwatch);
 
-        this.running = false;
-        this.display = display;
-        this.reset();
-        this.print(this.times);
-    } //BRAK ŚREDNIKA I PRZECINKA 
-    // OD RAZU KOLEJNA METODA reset() PRINT()
+								this.running = false;
+								this.display = display;
+								this.reset();
+								this.print(this.times);
+				} //BRAK ŚREDNIKA I PRZECINKA 
+				// OD RAZU KOLEJNE METODy reset() print() itd...
 
-    _createClass(Stopwatch, [{
-        key: 'reset',
-        value: function reset() {
-            this.times = {
-                minutes: 0,
-                seconds: 0,
-                miliseconds: 0
-            };
-        }
-        //metoda format zajmuje się przygotowaniem tekstu do wyświetlenia
+				_createClass(Stopwatch, [{
+								key: 'reset',
+								value: function reset() {
+												this.times = {
+																minutes: 0,
+																seconds: 0,
+																miliseconds: 0
+												};
+								}
+								//metoda format zajmuje się przygotowaniem tekstu do wyświetlenia
 
-    }, {
-        key: 'print',
-        value: function print() {
-            this.display.innerText = this.format(this.times);
-        }
-    }, {
-        key: 'format',
-        value: function format(times) {
-            return pad0(times.minutes) + ':' + pad0(times.seconds) + ':' + pad0(Math.floor(times.miliseconds));
-        }
-    }]);
+				}, {
+								key: 'print',
+								value: function print() {
+												this.display.innerText = this.format(this.times);
+								}
+				}, {
+								key: 'format',
+								value: function format(times) {
+												return pad0(times.minutes) + ':' + pad0(times.seconds) + ':' + pad0(Math.floor(times.miliseconds));
+								}
+				}, {
+								key: 'start',
+								value: function start() {
+												var _this = this;
 
-    return Stopwatch;
+												if (!this.running) {
+																this.running = true;
+																this.watch = setInterval(function () {
+																				return _this.step();
+																}, 10);
+												}
+								}
+				}, {
+								key: 'step',
+								value: function step() {
+												if (!this.running) return;
+												this.calculate();
+												this.print();
+								}
+				}, {
+								key: 'calculate',
+								value: function calculate() {
+												this.times.miliseconds += 1;
+												if (this.times.miliseconds >= 100) {
+																this.times.seconds += 1;
+																this.times.miliseconds = 0;
+												}
+												if (this.times.seconds >= 60) {
+																this.times.minutes += 1;
+																this.times.seconds = 0;
+												}
+								}
+				}, {
+								key: 'stop',
+								value: function stop() {
+												this.running = false;
+												clearInterval(this.watch);
+								}
+				}]);
+
+				return Stopwatch;
 }();
 
 var stopwatch = new Stopwatch(document.querySelector('.stopwatch'));
 
 var startButton = document.getElementById('start');
 startButton.addEventListener('click', function () {
-    return stopwatch.start();
+				return stopwatch.start();
 });
 
 var stopButton = document.getElementById('stop');
 stopButton.addEventListener('click', function () {
-    return stopwatch.stop();
+				return stopwatch.stop();
 });
 
 //pad0 ma za zadanie dodać zero do liczb jednocyfrowych
 function pad0(value) {
-    var result = value.toString();
-    if (result.length < 2) {
-        result = '0' + result;
-    }
-    return result;
+				var result = value.toString();
+				if (result.length < 2) {
+								result = '0' + result;
+				}
+				return result;
 };
--- script.js
+++ script.js
@@ -5,7 +5,7 @@ class Stopwatch {
         this.reset();
         this.print(this.times);
     }  //BRAK ŚREDNIKA I PRZECINKA 
-    // OD RAZU KOLEJNA METODA reset() PRINT()
+    // OD RAZU KOLEJNE METODy reset() print() itd...
 
     reset() {
         this.times = {
@@ -21,6 +21,32 @@ class Stopwatch {
 
 	format(times) {
         return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
+	}
+	start() {
+	    if (!this.running) {
+	        this.running = true;
+	        this.watch = setInterval(() => this.step(), 10);
+	    }
+	}
+	step() {
+	    if (!this.running) return;
+	    this.calculate();
+	    this.print();
+	}
+	calculate() {
+	    this.times.miliseconds += 1;
+	    if (this.times.miliseconds >= 100) {
+	        this.times.seconds += 1;
+	        this.times.miliseconds = 0;
+	    }
+	    if (this.times.seconds >= 60) {
+	        this.times.minutes += 1;
+	        this.times.seconds = 0;
+	    }
+	}
+	stop() {
+	    this.running = false;
+	    clearInterval(this.watch);
 	}
 
 }

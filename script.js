92e7c79c61c40d314ab84612e65b877eaabc5e89
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-05-20 22:46:47 +0200

    add initial state

--- script.js
+++ script.js
@@ -1,2 +1,47 @@
+class Stopwatch {
+    constructor(display) {
+        this.running = false;
+        this.display = display;
+        this.reset();
+        this.print(this.times);
+    }  //BRAK ŚREDNIKA I PRZECINKA 
+    // OD RAZU KOLEJNA METODA reset() PRINT()
 
+    reset() {
+        this.times = {
+            minutes: 0,
+            seconds: 0,
+            miliseconds: 0
+        };
+    }
+//metoda format zajmuje się przygotowaniem tekstu do wyświetlenia
+    print() {
+        this.display.innerText = this.format(this.times);
+	}
+
+	format(times) {
+        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
+	}
+
+}
+
+const stopwatch = new Stopwatch(
+document.querySelector('.stopwatch'));
+
+let startButton = document.getElementById('start');
+startButton.addEventListener('click', () => stopwatch.start());
+
+let stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', () => stopwatch.stop());
+
+//pad0 ma za zadanie dodać zero do liczb jednocyfrowych
+function pad0(value) {
+    let result = value.toString();
+    if (result.length < 2) {
+        result = '0' + result;
+    }
+    return result;
+};
+
+	
 	
